8002045a189b1fe9f2f94e56c85afefacddbb917
risefenixpuryfire@gmail.com <risefenixpuryfire@gmail.com>
2023-05-29 14:43:01 +0300

    .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-
+import promt
 def main():
      print("Welcome to the Brain Games!")
+     
+import prompt
+
+def welcome_user:
+	name = prompt.string('May I have your name? ')
 
 
 if __name__ == '__main__':
