c1221f7f7ae86d10e4e4ba41d8560c0952f46f07
arcwarden <risefenixpuryfire@gmail.com>
2023-05-30 19:51:42 +0300

    some debuggy stuffy stuf

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 import prompt
+
 from cli import welcome_user
 
 def main():
@@ -9,3 +11,4 @@ def main():
 if __name__ == '__main__':
      main()
      welcome_user()
+     
