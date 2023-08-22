d93ada0245fcd86f2c237a875bc7d4eec0a7236d
Sergei <risefenixpuryfire@gmail.com>
2023-08-22 20:04:49 +0300

    a problem in 3 phase

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import prompt
+from brain_games.scripts.cli import welcome_user
 
 def main():
     print("Welcome to the Brain Games!")
@@ -6,3 +8,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    welcome_user()
