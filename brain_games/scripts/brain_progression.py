1dcf13c6c4ecccc3d27cf25201610bc8766a16bf
Sergei <risefenixpuryfire@gmail.com>
2023-08-31 17:53:32 +0300

    s

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -3,7 +3,6 @@ from brain_games.engines.brain_progression_logic import  play_game
 
 def main():
     print("Welcome to the Brain Games!")
-    print("Welcome to the Brain Games!")
     play_game()
 
 if __name__ == '__main__':
