d299e8756d8d82ce77d6a8fab3d5343d140e140d
Sergei <risefenixpuryfire@gmail.com>
2023-09-05 14:07:49 +0300

    sd

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -1,21 +1,3 @@
-def play_round(start, step, slice_step, pop_step):
-    progression = list(range(start, 999, step))
-    sliced_progression = progression[0:slice_step + 1]
-
-    if pop_step > slice_step:
-        pop_step = slice_step
-
-    progression_pop = sliced_progression.pop(pop_step)
-    sliced_progression.insert(pop_step, '..')
-    final_progression = ' '.join(map(str, sliced_progression))
-    print_question(f"{final_progression}")
-
-    user_response = get_user_response()
-    print('Your answer:', user_response)
-
-    return str(user_response), progression_pop
-
-
 def get_user_response():  # allfile
     return input()
 
