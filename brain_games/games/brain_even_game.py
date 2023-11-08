55ab96e28d2ee078f567ea61595739266eb882f2
Sergei <risefenixpuryfire@gmail.com>
2023-11-08 22:06:07 +0300

    as

--- brain_games/games/brain_even_game.py
+++ brain_games/games/brain_even_game.py
@@ -1,13 +1,13 @@
 from random import randint
-from .constants import *
+from brain_games.games.constants import first_number_even,second_number_even
 
 description = 'Answer "yes" if the number is even, otherwise answer "no".'
-def is_even():
-    nonlocal generated_num_for_question
-    return True if generated_num_for_question % 2 == 0 else False
-
 def play_game():
     generated_num_for_question = randint(first_number_even, second_number_even)
+    def is_even():
+        nonlocal generated_num_for_question
+        return True if generated_num_for_question % 2 == 0 else False
+
     if is_even():
         operation = 'yes'
     else:
--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -22,7 +22,7 @@ def calculate_gcd(a, b):
     return a
 
 
-def generate_gcd_question():  # Измените имя функции
+def generate_gcd_question():
     first_gcd_num = randint(first_number_gcd, second_number_gcd)
     second_gcd_num = randint(first_number_gcd, second_number_gcd)
     answer = str(calculate_gcd(first_gcd_num, second_gcd_num))
